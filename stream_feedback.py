3a9e1a9cde6b09f1d0fa626bf7392290a241f72e
pptryna <22611147@students.uii.ac.id>
2024-07-25 20:16:17 +0700

    update all files

--- stream_feedback.py
+++ stream_feedback.py
@@ -19,10 +19,10 @@ def is_valid_integer(value):
         return False
 
 # Function to simulate prediction logic
-def predict(Age, Marital_Status, Occupation, Educational_Qualifications):
+def predict(Age, Gender, Marital_Status, Occupation, Monthly_Income, Educational_Qualifications, Family_size):
     # Placeholder logic for prediction
     # You can replace this with actual model prediction logic
-    input_data = [[int(Age), int(Marital_Status), int(Occupation), int(Educational_Qualifications)]]
+    input_data = [[int(Age), int(Gender), int(Marital_Status), int(Occupation), int(Monthly_Income), int(Educational_Qualifications), int(Family_size)]]
     feedback_prediction = feedback_model.predict(input_data)
     
     # Menentukan kategori feedback berdasarkan prediksi
@@ -39,20 +39,23 @@ st.title("Prediksi Feedback Customer")
 # Form for user input
 with st.form(key='feedback_form'):
     Age = st.text_input('Age', '1')
+    Gender = st.text_input('Gender', '0')
     Marital_Status = st.text_input('Marital Status', '0')
     Occupation = st.text_input('Occupation', '1')
+    Monthly_Income = st.text_input('Monthly Income', '0')
     Educational_Qualifications = st.text_input('Educational Qualifications', '0')
+    Family_size = st.text_input('Family Size', '1')
     
     # Submit button for the form
     submit_button = st.form_submit_button(label='Prediksi')
 
 # Validation and prediction logic after form submission
 if submit_button:
-    if (is_valid_integer(Age) and is_valid_integer(Marital_Status) and is_valid_integer(Occupation) and is_valid_integer(Educational_Qualifications)):
+    if (is_valid_integer(Age) and is_valid_integer(Gender) and is_valid_integer(Marital_Status) and is_valid_integer(Occupation) and is_valid_integer(Monthly_Income) and is_valid_integer(Educational_Qualifications) and is_valid_integer(Family_size)):
         st.success("All inputs are valid integers.")
         
         # Perform prediction
-        prediction = predict(Age, Marital_Status, Occupation, Educational_Qualifications)
+        prediction = predict(Age, Gender, Marital_Status, Occupation, Monthly_Income, Educational_Qualifications, Family_size)
         
         # Display prediction result
         if prediction == 'Positif':
